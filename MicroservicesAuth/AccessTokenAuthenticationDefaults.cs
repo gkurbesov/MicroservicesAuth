ccf11a5635230b89b050299e76124a6315f5e1b5
Gennady <ltrg67@gmail.com>
2021-08-25 22:40:50 +0300

    Добавил комментарии

--- MicroservicesAuth/AccessTokenAuthenticationDefaults.cs
+++ MicroservicesAuth/AccessTokenAuthenticationDefaults.cs
@@ -4,10 +4,22 @@ using System.Text;
 
 namespace MicroservicesAuth
 {
+    /// <summary>
+    /// Стандартные значения используемые в библиотеке
+    /// </summary>
     public static class AccessTokenAuthenticationDefaults
     {
+        /// <summary>
+        /// Заголовок head'ра который содержит данные для аутентификации
+        /// </summary>
         internal const string AuthorizationHeader = "Authorization";
+        /// <summary>
+        /// Название схемы аутентификации
+        /// </summary>
         public const string AuthenticationScheme = "AccessToken";
+        /// <summary>
+        /// Префикс токена аутентификации
+        /// </summary>
         public const string AuthorizationScheme = "Bearer";
     }
 }
