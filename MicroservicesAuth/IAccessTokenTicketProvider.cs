50fd65709ba01d48229e22068ef809af8a90c37c
Gennady <ltrg67@gmail.com>
2021-08-26 17:10:56 +0300

    Упростил интерфес

---
+++ MicroservicesAuth/IAccessTokenTicketProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicroservicesAuth
+{
+    public interface IAccessTokenTicketProvider
+    {
+        /// <summary>
+        /// Запрос тикета (билета) аутентификации по токену
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <returns></returns>
+        Task<AuthenticationTicket> GetTicketAsync(string accessToken);
+    }
+}
