7e21e1213671f33368c2f2bd847e2a42063d8ee1
Gennady <ltrg67@gmail.com>
2021-08-26 17:11:42 +0300

    Пост настройка опций

---
+++ MicroservicesAuth/PostConfigureAccessTokenAuthenticationOptions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroservicesAuth
+{
+    public class PostConfigureAccessTokenAuthenticationOptions : IPostConfigureOptions<AccessTokenAuthenticationOptions>
+    {
+        private readonly IAccessTokenTicketProvider accessTokenTicketProvider;
+
+        public PostConfigureAccessTokenAuthenticationOptions(IAccessTokenTicketProvider accessTokenTicketProvider)
+        {
+            this.accessTokenTicketProvider = accessTokenTicketProvider;
+        }
+
+        public void PostConfigure(string name, AccessTokenAuthenticationOptions options)
+        {
+            options.TicketProvider = options.TicketProvider ?? accessTokenTicketProvider;
+        }
+    }
+}
